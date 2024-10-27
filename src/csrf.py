3f33a376644052dc8b14d9e9eb772b68d7b8cff2
Aspect <90333100+Aspectise@users.noreply.github.com>
2024-10-27 14:56:29 +0200

    Update csrf.py

--- src/csrf.py
+++ src/csrf.py
@@ -2,7 +2,7 @@ import requests
 from rgbprint import Color
 
 def get(cookie) -> str:
-    response = requests.post("https://economy.roblox.com/", cookies = {".ROBLOSECURITY": cookie})
+    response = requests.post("https://auth.roblox.com/v2/logout", cookies = {".ROBLOSECURITY": cookie})
     xcsrf_token = response.headers.get("x-csrf-token")
     if not xcsrf_token:
         print(f"{Color(255, 0, 0)}ERROR{Color(255, 255, 255)} | An error occurred while getting the X-CSRF-TOKEN. Could be due to an invalid Roblox Cookie")
